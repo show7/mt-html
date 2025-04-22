885aa7d90564f258ce44470874071c860f94926e
Frank <2425913783@qq.com>
2025-04-22 17:16:49 +0800

    feat: 优化部分页面样式

--- js/tailwindcss.config.js
+++ js/tailwindcss.config.js
@@ -12,6 +12,20 @@ window.onload = () => {
           },
         },
       },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '1.25rem',
+        },
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          // '2xl': '1536px',
+        },
+      },
     },
     plugins: [
       function ({ addBase, theme }) {
